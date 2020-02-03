1ad6545bde8e1fcf98c450044e4a12439d8c2601
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2020-02-03 13:15:03 +0100

    Update vue_script.js
    Task 4-3

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,3 +1,17 @@
 
+function menuItem(name, kCal, containsGluten, containsLactose, img) {
+  this.name = name;
+  this.kCal = kCal;
+  this.containsGluten = containsGluten;
+  this.containsLactose = containsLactose;
+  this.img = img;
+  this.getName = funtion() {
+    return "name: " + this.name + ", kCal: " + this.kCal;
+  }
+}
+
+var menuArray = [];
+for (i = 0; i < food.length; i++) {
+    menuArray[i] = new MenuItem(food[i].name, food[i].kCal, food[i].lactose, food[i].gluten, food[i].img);
 
 
