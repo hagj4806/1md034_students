ee07fc9eaef5f5bb3adfb945b455251d083d4064
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2020-02-03 09:45:37 +0100

    Create menu.js
    menu

---
+++ public/js/menu.js
@@ -0,0 +1,42 @@
+let food = [
+    {
+        "name": "The Vegan Burger",
+        "kCal": 750,
+        "lactose": false,
+        "gluten": false,
+        "img": "https://www.wearesovegan.com/wp-content/uploads/2019/07/veganburger-h1-1170x1645.jpg"
+    },
+    {
+        "name": "The Veg Burger",
+        "kCal": 800,
+        "lactose": false,
+        "gluten": true,
+        "img": "https://cdn.sallysbakingaddiction.com/wp-content/uploads/2018/07/best-black-bean-burgers-2.jpg"
+    },
+    {
+        "name": "The Meat Burger",
+        "kCal": 1300,
+        "lactose": true,
+        "gluten": true,
+        "img": "https://www.santamariaworld.com/optimized/recipe-large/globalassets/_recipes/bbq/kentucky_burger.jpg?id=2357"
+    },
+
+    {
+        "name": "Umami Burger",
+        "kCal": 900,
+        "lactose": true,
+        "gluten": true,
+        "img": "https://www.fsrmagazine.com/sites/default/files/styles/story_image_720x430/public/2019-11/SBE.jpg?itok=QyswJQdq"
+    },
+
+    {
+        "name": "Stockholm Burger",
+        "kCal": 120000,
+        "lactose": true,
+        "gluten": false,
+        "img": "https://bigseventravel.com/wp-content/uploads/2019/03/Screenshot-2019-03-21-at-15.12.44.png"
+    }
+
+
+
+]
