87c45e0a7ec0c423474d896d73a5f6c129afd1d7
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2020-02-03 14:03:39 +0100

    Update vue_script.js
    "select burger"

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -10,8 +10,16 @@ function menuItem(name, kCal, containsGluten, containsLactose, img) {
   }
 }
 
-var menuArray = [];
-for (i = 0; i < food.length; i++) {
-    menuArray[i] = new MenuItem(food[i].name, food[i].kCal, food[i].lactose, food[i].gluten, food[i].img);
+var vm = new Vue({
+    el:'#Burger',
+    data: {
+        selBurger: "Select your burger " + Date(),
+        selBurg: "Select your Burger"
+        burgtext: "This is where you execute burger selection",
+        menuArray: menuArray
+
+    }
+  }
+});
 
 
