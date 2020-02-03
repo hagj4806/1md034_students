be0271d3c0921ef7d8fd36c50c241dae4a29304e
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2020-02-03 15:27:48 +0100

    Update vue_script.js
    Task 4-6

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,25 +1,11 @@
+const vm = new Vue ({
+el: '#myID',
+data: {
+	burger1: burger1,
+  burger2: burger2,
 
-function menuItem(name, kCal, containsGluten, containsLactose, img) {
-  this.name = name;
-  this.kCal = kCal;
-  this.containsGluten = containsGluten;
-  this.containsLactose = containsLactose;
-  this.img = img;
-  this.getName = funtion() {
-    return "name: " + this.name + ", kCal: " + this.kCal;
-  }
-}
+},
+})
 
-var vm = new Vue({
-    el:'#Burger',
-    data: {
-        selBurger: "Select your burger " + Date(),
-        selBurg: "Select your Burger"
-        burgtext: "This is where you execute burger selection",
-        menuArray: menuArray
-
-    }
-  }
-});
 
 
