4a270c35d3dc1d751a5e9907bc306c10d86004c4
Hanna <hanna.gjothlen@gmail.com>
2020-02-03 12:36:29 +0100

    doen

--- public/js/js_script.js
+++
@@ -1,6 +0,0 @@
-function menuItem(name, kCal, containing) {
-  this.name = name
-  this.kCal = kCal
-  this.name= containing
-  var prop = $('<img class="prop" src="/Images/prop.ico" />').attr('id', i);
-}
