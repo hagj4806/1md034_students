14d719e78a13f383c7f0e4e5b20e88c343b1774e
hagj4806 <43148234+hagj4806@users.noreply.github.com>
2020-02-10 14:02:27 +0100

    Update vue_script.js
    Task 4.8

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,14 +1,4 @@
-/*const vm = new Vue ({
-el: '#myID',
-data: {
-	burger1: burger1,
-  burger2: burger2,
-
-},
-})
-*/
-
-function MenuItem(name, kcal, containsGluten, containsLactos, img) {
+function menuItem(name, kcal, containsGluten, containsLactos, img) {
     this.name= name;
     this.kcal=kcal;
     this.containsGluten = containsGluten;
@@ -18,6 +8,76 @@ function MenuItem(name, kcal, containsGluten, containsLactos, img) {
         return "Name: " + this.name + ", kCal:  " + this.kcal;
     }
 }
+var menu = [];
+for (i = 0; i < food.length; i++) {
+    menu[i] = new menuItem(food[i].name, food[i].kCal, food[i].lactose, food[i].gluten, food[i].img);
+}
+
+var vm = new Vue({
+    el:'#menu',
+    data: {
+        selectBurg: "Select Burger " + Date(),
+        selBurg: "Select Burger",
+        burgtext: "This is where you execute burger selection",
+        menu: menu
+
+    }
+});
+
+let food = [
+    {
+        "name": "The Vegan Burger",
+        "kCal": 750,
+        "lactose": false,
+        "gluten": false,
+        "img": "https://www.wearesovegan.com/wp-content/uploads/2019/07/veganburger-h1-1170x1645.jpg"
+    },
+    {
+        "name": "The Veg Burger",
+        "kCal": 800,
+        "lactose": false,
+        "gluten": true,
+        "img": "https://cdn.sallysbakingaddiction.com/wp-content/uploads/2018/07/best-black-bean-burgers-2.jpg"
+    },
+    {
+        "name": "The Meat Burger",
+        "kCal": 1300,
+        "lactose": true,
+        "gluten": true,
+        "img": "https://www.santamariaworld.com/optimized/recipe-large/globalassets/_recipes/bbq/kentucky_burger.jpg?id=2357"
+    },
+
+    {
+        "name": "Umami Burger",
+        "kCal": 900,
+        "lactose": true,
+        "gluten": true,
+        "img": "https://www.fsrmagazine.com/sites/default/files/styles/story_image_720x430/public/2019-11/SBE.jpg?itok=QyswJQdq"
+    },
+
+    {
+        "name": "Stockholm Burger",
+        "kCal": 120000,
+        "lactose": true,
+        "gluten": false,
+        "img": "https://bigseventravel.com/wp-content/uploads/2019/03/Screenshot-2019-03-21-at-15.12.44.png"
+    }
+
+
+
+]
+
+
+/*const vm = new Vue ({
+el: '#myID',
+data: {
+	burger1: burger1,
+  burger2: burger2,
+
+},
+})
+*/
+/*
 
 let burger1 = new menuItem('Vegan', '500', true, true);
 let burger2 = new menuItem('Veg', '600', true, false);
@@ -37,7 +97,7 @@ const vm = new Vue ({
 
 })
 
-/*   in html
+  in html
 <script src="https://vuejs.org/js/vue.js"></script>
 <div id="burger">
     <h1>Välj en burgare</h1>
@@ -51,6 +111,3 @@ const vm = new Vue ({
     </span>
    </div>
 */
-
-
-
